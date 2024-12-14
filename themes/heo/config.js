1212b26f5b4f295459b0111342a3390d49aaf91c
SmallsmallQ <140693277+SmallsmallQ@users.noreply.github.com>
2024-12-15 01:11:57 +0800

    2

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -39,21 +39,21 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '你好！我是龚运博',
+    '📚 法学与科技的探索者',
+    '🤝 热爱分享与实践',
+    '💻 专注科技与法律的交叉点',
+    '🌟 持续学习与创新',
+    '📌 探索数字化未来',
+    '🌍 共同塑造智慧社会'
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/SmallsmallQ',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://www.smallq.top',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
