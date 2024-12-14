a5c49e32add00786c23c7aceae08ec94dbc293f0
SmallsmallQ <140693277+SmallsmallQ@users.noreply.github.com>
2024-12-15 01:34:09 +0800

    222

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到龚运博的个人网站', url: 'https://www.smallq.top/' },
-    { title: '关于我', url: 'https://docs.tangly1024.com' }
+    { title: '关于我', url: 'https://www.smallq.top/article/115c9f55-1e8c-81c5-b129-c64978c0e28c' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -58,10 +58,10 @@ const CONFIG = {
 
   
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '崇实学辅',
+  HEO_SOCIAL_CARD_TITLE_2: '欢饮访问我创建的崇实书院学业辅导中心网站',
+  HEO_SOCIAL_CARD_TITLE_3: '点击观看',
+  HEO_SOCIAL_CARD_URL: 'https://www.csxf.online/about',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
